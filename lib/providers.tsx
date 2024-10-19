99c3207b434d7ebb8b87f2ba37e9fcf3f5364f7c
Sergiy Dybskiy <s@serg.tech>
2024-10-18 21:23:16 -0700

    first commit

---
+++ lib/providers.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { config } from "@/lib/wagmi";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
+  return (
+    <DynamicContextProvider
+      theme="auto"
+      settings={{
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_LABS_ENVIRONMENT_ID,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
+  );
+}
