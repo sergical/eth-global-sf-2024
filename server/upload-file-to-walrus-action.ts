611b3727975f9f63cd1850b009c7e6f8f8529735
Sergiy Dybskiy <s@serg.tech>
2024-10-19 16:36:30 -0700

    fixing upload

--- server/upload-file-to-walrus-action.ts
+++ server/upload-file-to-walrus-action.ts
@@ -19,16 +19,35 @@ export default async function uploadFileToWalrusAction(formData: FormData) {
     }
   );
 
-  const {
-    newlyCreated: {
-      blobObject: { blobId: blob_id },
-    },
-  } = await response.json();
+  console.log(response);
 
-  await supabase.from("user_blobs").insert({
+  const json = await response.json();
+
+  console.log(json);
+
+  let blob_id = "";
+
+  if (json.alreadyCertified) {
+    blob_id = json.alreadyCertified.blobId;
+  } else {
+    blob_id = json.newlyCreated.blobObject.blobId;
+  }
+
+  console.log(`Blob ID: ${blob_id}`);
+
+  console.log(
+    `inserting into supabase blob_id: ${blob_id} address: ${address}`
+  );
+
+  const { error } = await supabase.from("user_blobs").insert({
     user_id: address,
     blob_id: blob_id,
   });
+
+  if (error) {
+    console.error(error);
+    return { success: false, error: error.message };
+  }
 
   return { success: true, blob_id: blob_id };
 }
